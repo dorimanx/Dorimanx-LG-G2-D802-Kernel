aa0bba5ec2ab55bd51726280f85e0c27d2c30b4e
Antti P Miettinen <amiettinen@nvidia.com>
2011-12-27 12:09:56 +0200

    cpufreq: Export user_policy min/max
    Add sysfs nodes for user_policy min and max settings.
    
    Bug 888312
    
    Change-Id: Ife8ec07f8faebc0c04e7ecf6e24976a311bad73b
    Reviewed-on: http://git-master/r/72201
    Signed-off-by: Antti P Miettinen <amiettinen@nvidia.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/75882
    Reviewed-by: Automatic_Commit_Validation_User
    
    Rebase-Id: Rf145967a22a5d871d01bb1334a2e7d366a6612fd

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -455,6 +455,8 @@ show_one(scaling_min_freq, min);
 show_one(scaling_max_freq, max);
 show_one(scaling_cur_freq, cur);
 show_one(cpu_utilization, util);
+show_one(policy_min_freq, user_policy.min);
+show_one(policy_max_freq, user_policy.max);
 
 static int __cpufreq_set_policy(struct cpufreq_policy *data,
 				struct cpufreq_policy *policy);
@@ -762,6 +764,8 @@ cpufreq_freq_attr_rw(scaling_setspeed);
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 define_one_global_rw(vdd_levels);
 #endif
+cpufreq_freq_attr_ro(policy_min_freq);
+cpufreq_freq_attr_ro(policy_max_freq);
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -776,6 +780,8 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
+	&policy_min_freq.attr,
+	&policy_max_freq.attr,
 	NULL
 };
 
