6415321a0b3ccffd933c6eddc4d023da8527216d
Theodore Ts'o <tytso@mit.edu>
2012-05-28 17:49:54 -0400

    ext4: return ENOMEM when mounts fail due to lack of memory
    This is a port of the ext3 commit: 4569cd1b0d9
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -3773,6 +3773,7 @@ static int ext4_fill_super(struct super_block *sb, void *data, int silent)
 					  GFP_KERNEL);
 	if (sbi->s_group_desc == NULL) {
 		ext4_msg(sb, KERN_ERR, "not enough memory");
+		ret = -ENOMEM;
 		goto failed_mount;
 	}
 
@@ -3830,6 +3831,7 @@ static int ext4_fill_super(struct super_block *sb, void *data, int silent)
 	}
 	if (err) {
 		ext4_msg(sb, KERN_ERR, "insufficient memory");
+		ret = err;
 		goto failed_mount3;
 	}
 
