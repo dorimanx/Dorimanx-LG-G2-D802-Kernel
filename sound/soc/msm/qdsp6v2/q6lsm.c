b45a4d415364a23cc1d3ad37d63a6ea75021a888
Venkat Sudhir <vsudhir@codeaurora.org>
2013-07-14 05:03:46 -0700

    ASoC: msm: qdsp6v2: Fix deadlock due to mutex unlock
    Mutex is not unlocked in one of the failure case
    in sound model buffer allocation function. This
    causes deadlock if the failure condition occurs.
    This change handles the mutex unlock correctly in
    failure case.
    
    Signed-off-by: Venkat Sudhir <vsudhir@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sound/soc/msm/qdsp6v2/q6lsm.c
+++ sound/soc/msm/qdsp6v2/q6lsm.c
@@ -749,7 +749,6 @@ int q6lsm_snd_model_buf_alloc(struct lsm_client *client, size_t len)
 		if (IS_ERR_OR_NULL(client->sound_model.client)) {
 			pr_err("%s: ION create client for AUDIO failed\n",
 			       __func__);
-			mutex_unlock(&client->cmd_lock);
 			goto fail;
 		}
 		client->sound_model.handle =
@@ -768,7 +767,6 @@ int q6lsm_snd_model_buf_alloc(struct lsm_client *client, size_t len)
 		if (rc) {
 			pr_err("%s: ION get physical mem failed, rc%d\n",
 			       __func__, rc);
-			mutex_unlock(&client->cmd_lock);
 			goto fail;
 		}
 
@@ -777,7 +775,6 @@ int q6lsm_snd_model_buf_alloc(struct lsm_client *client, size_t len)
 				   client->sound_model.handle);
 		if (IS_ERR_OR_NULL(client->sound_model.data)) {
 			pr_err("%s: ION memory mapping failed\n", __func__);
-			mutex_unlock(&client->cmd_lock);
 			goto fail;
 		}
 		memset(client->sound_model.data, 0, len);
