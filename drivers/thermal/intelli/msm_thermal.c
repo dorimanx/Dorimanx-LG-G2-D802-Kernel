535e8b63aaf8c045140064d9b34851f72b6ba9c3
WhatHub <modernwhatever@gmail.com>
2013-10-30 14:56:50 +0100

    intelli-thermal: adjust temp

--- drivers/thermal/intelli/msm_thermal.c
+++ drivers/thermal/intelli/msm_thermal.c
@@ -43,11 +43,11 @@ static int enabled;
 static struct msm_thermal_data msm_thermal_info = {
 	.sensor_id = 5,
 	.poll_ms = 250,
-	.limit_temp_degC = 80,
+	.limit_temp_degC = 70,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
 	.freq_control_mask = 0xf,
-	.core_limit_temp_degC = 85,
+	.core_limit_temp_degC = 80,
 	.core_temp_hysteresis_degC = 10,
 	.core_control_mask = 0xe,
 };
