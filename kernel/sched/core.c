b13f30087790633e837200630948dc417e366b75
Thomas Gleixner <tglx@linutronix.de>
2013-11-21 12:41:44 +0100

    sched: Expose preempt_schedule_irq()
    Tony reported that aa0d53260596 ("ia64: Use preempt_schedule_irq")
    broke PREEMPT=n builds on ia64.
    
    Ok, wrapped my brain around it. I tripped over the magic asm foo which
    has a single need_resched check and schedule point for both sys call
    return and interrupt return.
    
    So you need the schedule_preempt_irq() for kernel preemption from
    interrupt return while on a normal syscall preemption a schedule would
    be sufficient. But using schedule_preempt_irq() is not harmful here in
    any way. It just sets the preempt_active bit also in cases where it
    would not be required.
    
    Even on preempt=n kernels adding the preempt_active bit is completely
    harmless. So instead of having an extra function, moving the existing
    one out of the ifdef PREEMPT looks like the sanest thing to do.
    
    It would also allow getting rid of various other sti/schedule/cli asm
    magic in other archs.
    
    Reported-and-Tested-by: Tony Luck <tony.luck@gmail.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3180,6 +3180,7 @@ asmlinkage void __sched notrace preempt_schedule(void)
 	} while (need_resched());
 }
 EXPORT_SYMBOL(preempt_schedule);
+#endif /* CONFIG_PREEMPT */
 
 /*
  * this is the entry point to schedule() from kernel preemption
@@ -3208,8 +3209,6 @@ asmlinkage void __sched preempt_schedule_irq(void)
 		barrier();
 	} while (need_resched());
 }
-
-#endif /* CONFIG_PREEMPT */
 
 int default_wake_function(wait_queue_t *curr, unsigned mode, int wake_flags,
 			  void *key)
