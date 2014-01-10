9d2629dfe79a6bd47eaa38bb1147f5f21c9c4a45
Saket Saurabh <ssaurabh@codeaurora.org>
2014-01-10 16:14:27 +0530

    USB: msm_otg: Fix race between PM resume and charger detection
    When device is in system suspend and USB charger is disconnected,
    then due to VBUS clear IRQ from PMIC, the device will be PM resumed.
    Now shortly if PMIC driver detects valid VBUS it will handle VBUS
    set condition,notifies to msm_otg and msm_otg will kick in charger
    detection mechanism. During PM resume, in dmp_complete() it will
    call a pm_runtime_put_sync(), which will immediately kick off the
    PM runtime idle and suspend routines.
    dpm_complete()
      ->device_complete()
        ->pm_runtime_put_sync()
          ->rpm_idle()
            ->rpm_suspend()
    
    The PM resume also runs in a separate worker queue, so it is not
    guaranteed that it will finish executing before we finish the charger
    detection stage. During this case there is a race if dpm_complete()
    returns before charger detection starts. Return of dmp_cpmplete()
    before charger detection will make USB to enter LPM. As USB will be
    in LPM now and if charger detection starts then it will lead to
    unclocked access.
    
    Fixing the issue by handling the pm usage count using the variable
    pm_done in msm_otg. When USB charger is disconnected and msm_otg
    processes the disconnect event it marks the pm_done to true. Later
    when USB charger is connected , mark the pm_done to false in
    msm_otg_runtime_resume(). If pm_done is true increment the pm counter
    using pm_runtime_get_sync. This handles the race case when PM resume
    thread returns before the charger detection starts.
    
    CRs-Fixed: 599143
    Change-Id: I6de19fe850e24efa03ea71c7d14e388c15cf2f97
    Signed-off-by: Saket Saurabh <ssaurabh@codeaurora.org>

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009-2013, Linux Foundation. All rights reserved.
+/* Copyright (c) 2009-2014, Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -2613,6 +2613,8 @@ static void msm_otg_sm_work(struct work_struct *w)
 	bool work = 0, srp_reqd, dcp;
 
 	pm_runtime_resume(otg->phy->dev);
+	if (motg->pm_done)
+		pm_runtime_get_sync(otg->phy->dev);
 	pr_debug("%s work\n", otg_state_string(otg->phy->state));
 	switch (otg->phy->state) {
 	case OTG_STATE_UNDEFINED:
@@ -2758,6 +2760,7 @@ static void msm_otg_sm_work(struct work_struct *w)
 			 */
 			pm_runtime_mark_last_busy(otg->phy->dev);
 			pm_runtime_autosuspend(otg->phy->dev);
+			motg->pm_done = 1;
 		}
 		break;
 	case OTG_STATE_B_SRP_INIT:
@@ -4927,6 +4930,7 @@ static int msm_otg_runtime_resume(struct device *dev)
 
 	dev_dbg(dev, "OTG runtime resume\n");
 	pm_runtime_get_noresume(dev);
+	motg->pm_done = 0;
 	return msm_otg_resume(motg);
 }
 #endif
@@ -4954,6 +4958,7 @@ static int msm_otg_pm_resume(struct device *dev)
 
 	dev_dbg(dev, "OTG PM resume\n");
 
+	motg->pm_done = 0;
 	atomic_set(&motg->pm_suspended, 0);
 	if (motg->async_int || motg->sm_work_pending) {
 		pm_runtime_get_noresume(dev);
