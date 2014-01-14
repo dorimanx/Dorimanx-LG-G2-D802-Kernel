bd8e39c7b72707630ae65a25f1e25b4b79f94e0d
dorimanx <yuri@bynet.co.il>
2014-01-14 18:12:28 +0200

    Added Power Suspend code. by Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Code changes are from furnace kernel source!
    
    http://forum.xda-developers.com/showthread.php?t=2572992
    
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/tree/furnace-kernel

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -11,6 +11,10 @@
 #include <linux/pm_wakeup.h>
 #include <linux/zwait.h>
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #include "power.h"
 
 static suspend_state_t autosleep_state;
@@ -106,8 +110,14 @@ int pm_autosleep_set_state(suspend_state_t state)
 		pm_wakep_autosleep_enabled(true);
 		zw_queue_up_suspend_work(state);
 		queue_up_suspend_work();
+#ifdef CONFIG_POWERSUSPEND
+                set_power_suspend_state_hook(POWER_SUSPEND_ACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
+#endif
 	} else {
 		pm_wakep_autosleep_enabled(false);
+#ifdef CONFIG_POWERSUSPEND
+                set_power_suspend_state_hook(POWER_SUSPEND_INACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
+#endif
 	}
 
 	mutex_unlock(&autosleep_lock);
