97eaf421faad086421d799e50cb8c9002cb5ee0c
dorimanx <yuri@bynet.co.il>
2014-01-14 16:09:29 +0200

    CPU OC to 2.496Ghz! + Freq limit code + Higher Flash voltage + UV on CPU
    and other small Config relocate
    
    Code changes are from furnace kernel source!
    
    http://forum.xda-developers.com/showthread.php?t=2572992
    
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/tree/furnace-kernel

--- arch/arm/mach-msm/acpuclock-8974.c
+++ arch/arm/mach-msm/acpuclock-8974.c
@@ -56,7 +56,11 @@ static struct scalable scalable[] __initdata = {
 		.hfpll_phys_base = 0xF908A000,
 		.l2cpmr_iaddr = 0x4501,
 		.sec_clk_sel = 2,
-		.vreg[VREG_CORE] = { "krait0",     1140000 },
+#ifdef CONFIG_CPU_OVERCLOCK
+		.vreg[VREG_CORE] = { "krait0",     1200000 },
+#else
+		.vreg[VREG_CORE] = { "krait0",     1100000 },
+#endif
 		.vreg[VREG_MEM]  = { "krait0_mem", 1050000 },
 		.vreg[VREG_DIG]  = { "krait0_dig", LVL_HIGH },
 		.vreg[VREG_HFPLL_A] = { "krait0_hfpll", 1800000 },
@@ -65,7 +69,11 @@ static struct scalable scalable[] __initdata = {
 		.hfpll_phys_base = 0xF909A000,
 		.l2cpmr_iaddr = 0x5501,
 		.sec_clk_sel = 2,
-		.vreg[VREG_CORE] = { "krait1",     1140000 },
+#ifdef CONFIG_CPU_OVERCLOCK
+		.vreg[VREG_CORE] = { "krait1",     1200000 },
+#else
+		.vreg[VREG_CORE] = { "krait1",     1100000 },
+#endif
 		.vreg[VREG_MEM]  = { "krait1_mem", 1050000 },
 		.vreg[VREG_DIG]  = { "krait1_dig", LVL_HIGH },
 		.vreg[VREG_HFPLL_A] = { "krait1_hfpll", 1800000 },
@@ -74,7 +82,11 @@ static struct scalable scalable[] __initdata = {
 		.hfpll_phys_base = 0xF90AA000,
 		.l2cpmr_iaddr = 0x6501,
 		.sec_clk_sel = 2,
-		.vreg[VREG_CORE] = { "krait2",     1140000 },
+#ifdef CONFIG_CPU_OVERCLOCK
+		.vreg[VREG_CORE] = { "krait2",     1200000 },
+#else
+		.vreg[VREG_CORE] = { "krait2",     1100000 },
+#endif
 		.vreg[VREG_MEM]  = { "krait2_mem", 1050000 },
 		.vreg[VREG_DIG]  = { "krait2_dig", LVL_HIGH },
 		.vreg[VREG_HFPLL_A] = { "krait2_hfpll", 1800000 },
@@ -83,7 +95,11 @@ static struct scalable scalable[] __initdata = {
 		.hfpll_phys_base = 0xF90BA000,
 		.l2cpmr_iaddr = 0x7501,
 		.sec_clk_sel = 2,
-		.vreg[VREG_CORE] = { "krait3",     1140000 },
+#ifdef CONFIG_CPU_OVERCLOCK
+		.vreg[VREG_CORE] = { "krait3",     1200000 },
+#else
+		.vreg[VREG_CORE] = { "krait3",     1100000 },
+#endif
 		.vreg[VREG_MEM]  = { "krait3_mem", 1050000 },
 		.vreg[VREG_DIG]  = { "krait3_dig", LVL_HIGH },
 		.vreg[VREG_HFPLL_A] = { "krait3_hfpll", 1800000 },
@@ -1496,8 +1512,9 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs0[] __initdata = {
 	{ 0, { 2188800, HFPLL, 1, 114 }, L2(19), 1085000, 659 },
 	{ 1, { 2265600, HFPLL, 1, 118 }, L2(19), 1100000, 691 },
 #ifdef CONFIG_CPU_OC
-	{ 0, { 2342400, HFPLL, 1, 122 }, L2(19), 1115000, 724 },
-	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1130000, 758 },
+	{ 1, { 2342400, HFPLL, 1, 122 }, L2(19), 1115000, 714 },
+	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1130000, 738 },
+	{ 1, { 2496000, HFPLL, 1, 130 }, L2(19), 1145000, 761 },
 #endif
 	{ 0, { 0 } }
 };
@@ -1531,8 +1548,9 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs1[] __initdata = {
 	{ 0, { 2188800, HFPLL, 1, 114 }, L2(19), 1060000, 659 },
 	{ 1, { 2265600, HFPLL, 1, 118 }, L2(19), 1075000, 691 },
 #ifdef CONFIG_CPU_OC
-	{ 0, { 2342400, HFPLL, 1, 122 }, L2(19), 1090000, 724 },
-	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1105000, 758 },
+	{ 1, { 2342400, HFPLL, 1, 122 }, L2(19), 1090000, 714 },
+	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1105000, 738 },
+	{ 1, { 2496000, HFPLL, 1, 130 }, L2(19), 1120000, 761 },
 #endif
 	{ 0, { 0 } }
 };
@@ -1566,8 +1584,9 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs2[] __initdata = {
 	{ 0, { 2188800, HFPLL, 1, 114 }, L2(19), 1035000, 659 },
 	{ 1, { 2265600, HFPLL, 1, 118 }, L2(19), 1050000, 691 },
 #ifdef CONFIG_CPU_OC
-	{ 0, { 2342400, HFPLL, 1, 122 }, L2(19), 1065000, 724 },
-	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1080000, 758 },
+	{ 1, { 2342400, HFPLL, 1, 122 }, L2(19), 1065000, 714 },
+	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1080000, 738 },
+	{ 1, { 2496000, HFPLL, 1, 130 }, L2(19), 1095000, 761 },
 #endif
 	{ 0, { 0 } }
 };
@@ -1601,8 +1620,9 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs3[] __initdata = {
 	{ 0, { 2188800, HFPLL, 1, 114 }, L2(19), 1010000, 659 },
 	{ 1, { 2265600, HFPLL, 1, 118 }, L2(19), 1025000, 691 },
 #ifdef CONFIG_CPU_OC
-	{ 0, { 2342400, HFPLL, 1, 122 }, L2(19), 1040000, 724 },
-	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1050000, 758 },
+	{ 1, { 2342400, HFPLL, 1, 122 }, L2(19), 1040000, 714 },
+	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1050000, 738 },
+	{ 1, { 2496000, HFPLL, 1, 130 }, L2(19), 1070000, 761 },
 #endif
 	{ 0, { 0 } }
 };
@@ -1636,8 +1656,9 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs4[] __initdata = {
 	{ 0, { 2188800, HFPLL, 1, 114 }, L2(19),  985000, 659 },
 	{ 1, { 2265600, HFPLL, 1, 118 }, L2(19), 1000000, 691 },
 #ifdef CONFIG_CPU_OC
-	{ 0, { 2342400, HFPLL, 1, 122 }, L2(19), 1015000, 724 },
-	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1025000, 758 },
+	{ 1, { 2342400, HFPLL, 1, 122 }, L2(19), 1015000, 714 },
+	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1030000, 738 },
+	{ 1, { 2496000, HFPLL, 1, 130 }, L2(19), 1045000, 761 },
 #endif
 	{ 0, { 0 } }
 };
@@ -1671,8 +1692,9 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs5[] __initdata = {
 	{ 0, { 2188800, HFPLL, 1, 114 }, L2(19),  965000, 659 },
 	{ 1, { 2265600, HFPLL, 1, 118 }, L2(19),  975000, 691 },
 #ifdef CONFIG_CPU_OC
-	{ 0, { 2342400, HFPLL, 1, 122 }, L2(19),  990000, 724 },
-	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1000000, 758 },
+	{ 1, { 2342400, HFPLL, 1, 122 }, L2(19),  990000, 714 },
+	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19), 1005000, 738 },
+	{ 1, { 2496000, HFPLL, 1, 130 }, L2(19), 1020000, 761 },
 #endif
 	{ 0, { 0 } }
 };
@@ -1706,8 +1728,9 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs6[] __initdata = {
 	{ 0, { 2188800, HFPLL, 1, 114 }, L2(19),  940000, 659 },
 	{ 1, { 2265600, HFPLL, 1, 118 }, L2(19),  950000, 691 },
 #ifdef CONFIG_CPU_OC
-	{ 0, { 2342400, HFPLL, 1, 122 }, L2(19),  960000, 724 },
-	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19),  975000, 758 },
+	{ 0, { 2342400, HFPLL, 1, 122 }, L2(19),  965000, 714 },
+	{ 1, { 2419200, HFPLL, 1, 126 }, L2(19),  980000, 738 },
+	{ 1, { 2496000, HFPLL, 1, 130 }, L2(19),  995000, 761 },
 #endif
 	{ 0, { 0 } }
 };
