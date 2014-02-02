9db3e66105d824938bff4fddc999fc801ff42f7d
dorimanx <yuri@bynet.co.il>
2014-02-02 17:53:03 +0200

    Enabled WIFI wakelocks, but reduced them + enabled powersuspend >>>
    To Save power on screen OFF.
    
    from dmesg on screen off
    <4>dhd_early_suspend: enter
    <4>dhd_set_suspend: force extra Suspend setting
    
    on screen on
    <4>dhd_late_resume: enter
    <4>dhd_set_suspend: Remove extra suspend setting
    
    using powersuspend code structure.
    
    This should save some more power when WIFI is ON and allow fast notifications
    from SYNC and apps.

--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux.c
@@ -44,6 +44,7 @@
 #include <linux/fs.h>
 #include <linux/ip.h>
 #include <net/addrconf.h>
+#include <linux/powersuspend.h>
 
 #include <asm/uaccess.h>
 #include <asm/unaligned.h>
@@ -399,7 +400,7 @@ typedef struct dhd_info {
 	dhd_attach_states_t dhd_state;
 
 #if defined(CONFIG_HAS_EARLYSUSPEND) && defined(DHD_USE_EARLYSUSPEND)
-	struct early_suspend early_suspend;
+	struct power_suspend early_suspend;
 #endif /* CONFIG_HAS_EARLYSUSPEND && DHD_USE_EARLYSUSPEND */
 
 #ifdef ARP_OFFLOAD_SUPPORT
@@ -3926,10 +3927,10 @@ dhd_attach(osl_t *osh, struct dhd_bus *bus, uint bus_hdrlen)
 #endif /* (LINUX_VERSION >= 2.6.27 && LINUX_VERSION <= 2.6.39 && CONFIG_PM_SLEEP */
 
 #if defined(CONFIG_HAS_EARLYSUSPEND) && defined(DHD_USE_EARLYSUSPEND)
-	dhd->early_suspend.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 20;
+/*	dhd->early_suspend.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 20; */
 	dhd->early_suspend.suspend = dhd_early_suspend;
 	dhd->early_suspend.resume = dhd_late_resume;
-	register_early_suspend(&dhd->early_suspend);
+	register_power_suspend(&dhd->early_suspend);
 	dhd_state |= DHD_ATTACH_STATE_EARLYSUSPEND_DONE;
 #endif /* CONFIG_HAS_EARLYSUSPEND && DHD_USE_EARLYSUSPEND */
 
@@ -5649,7 +5650,7 @@ void dhd_detach(dhd_pub_t *dhdp)
 #if defined(CONFIG_HAS_EARLYSUSPEND) && defined(DHD_USE_EARLYSUSPEND)
 	if (dhd->dhd_state & DHD_ATTACH_STATE_EARLYSUSPEND_DONE) {
 		if (dhd->early_suspend.suspend)
-			unregister_early_suspend(&dhd->early_suspend);
+			unregister_power_suspend(&dhd->early_suspend);
 	}
 #endif /* CONFIG_HAS_EARLYSUSPEND && DHD_USE_EARLYSUSPEND */
 
