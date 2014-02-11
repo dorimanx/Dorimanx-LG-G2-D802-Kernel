e26ce1d8755b650aae012eef4c17e7811a03d0b7
dorimanx <yuri@bynet.co.il>
2014-02-11 22:33:32 +0200

    Removed intelli/msm_thermal freq mask and core mask from userspace.
    seems like if this settings changed, driver not responce any more.

--- drivers/thermal/intelli/msm_thermal.c
+++ drivers/thermal/intelli/msm_thermal.c
@@ -67,12 +67,14 @@ static uint32_t hist_index = 0;
 module_param_named(poll_ms, msm_thermal_info.poll_ms, uint, 0664);
 module_param_named(limit_temp_degC, msm_thermal_info.limit_temp_degC,
 			int, 0664);
-module_param_named(freq_control_mask, msm_thermal_info.freq_control_mask,
-			uint, 0664);
 module_param_named(core_limit_temp_degC, msm_thermal_info.core_limit_temp_degC,
 			int, 0664);
+#if 0
+module_param_named(freq_control_mask, msm_thermal_info.freq_control_mask,
+			uint, 0664);
 module_param_named(core_control_mask, msm_thermal_info.core_control_mask,
 			uint, 0664);
+#endif
 
 module_param_named(thermal_limit_high, limit_idx_high, int, 0644);
 module_param_named(thermal_limit_low, limit_idx_low, int, 0644);
