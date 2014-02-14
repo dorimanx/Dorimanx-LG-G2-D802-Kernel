b8c9f7424dbfd0432ec9cf1de62f533bcc53fe37
dorimanx <yuri@bynet.co.il>
2014-02-14 03:42:11 +0200

    ARM: VFP fix proc fs entry not loaded, FS still not online.

--- arch/arm/vfp/vfpmodule.c
+++ arch/arm/vfp/vfpmodule.c
@@ -749,9 +749,6 @@ static int __init vfp_init(void)
 {
 	unsigned int vfpsid;
 	unsigned int cpu_arch = cpu_architecture();
-#ifdef CONFIG_PROC_FS
-	static struct proc_dir_entry *procfs_entry;
-#endif
 	if (cpu_arch >= CPU_ARCH_ARMv6)
 		on_each_cpu(vfp_enable, NULL, 1);
 
@@ -826,7 +823,18 @@ static int __init vfp_init(void)
 		}
 	}
 
+#ifdef CONFIG_MACH_LGE
+	printk(KERN_INFO "VFP support v0.3: Return ");
+#endif
+
+	return 0;
+}
+
+static int __init vfp_bounce_proc_entry(void)
+{
 #ifdef CONFIG_PROC_FS
+        static struct proc_dir_entry *procfs_entry;
+
 	procfs_entry = create_proc_entry("cpu/vfp_bounce", S_IRUGO, NULL);
 
 	if (procfs_entry)
@@ -834,11 +842,8 @@ static int __init vfp_init(void)
 	else
 		pr_err("Failed to create procfs node for VFP bounce reporting\n");
 #endif
-#ifdef CONFIG_MACH_LGE
-	printk(KERN_INFO "VFP support v0.3: Return ");
-#endif
-
 	return 0;
 }
+rootfs_initcall(vfp_bounce_proc_entry);
 
 core_initcall(vfp_init);
