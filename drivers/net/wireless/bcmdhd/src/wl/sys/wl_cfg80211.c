d87d127ff2a3105f15e280eb7dfc7c07c3ed5515
dorimanx <yuri@bynet.co.il>
2014-02-14 02:19:24 +0200

    net: wireless: bcmdhd: cancel current PNO if connection will happen
    	Issue : Manta: Lack of WLS causes PNO problem
    	Problem : we found that race condition happens
    		between connection and PNO sometimes.
    		Connection is failed due to this race condition.
    	Solution: we need to cancel PNO explicitly if the connection
    	request start in DHD driver.
    	Bug : 11174374
    Signed-off-by: Ecco Park <eccopark@broadcom.com>
    
    Change-Id: I7bf81d127901a74e9f47d6cc4ee7b265c6936dc3
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/net/wireless/bcmdhd/src/wl/sys/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/src/wl/sys/wl_cfg80211.c
@@ -390,6 +390,7 @@ static s32 wl_notify_escan_complete(struct wl_priv *wl,
 static s32 wl_cfg80211_tdls_oper(struct wiphy *wiphy, struct net_device *dev,
 	u8 *peer, enum nl80211_tdls_operation oper);
 #endif /* LINUX_VERSION > KERNEL_VERSION(3,2,0) || WL_COMPAT_WIRELESS */
+static int wl_cfg80211_sched_scan_stop(struct wiphy *wiphy, struct net_device *dev);
 
 /*
  * event & event Q handlers for cfg80211 interfaces
@@ -3685,6 +3686,11 @@ wl_cfg80211_connect(struct wiphy *wiphy, struct net_device *dev,
 	wl->block_gon_req_tx_count = 0;
 	wl->block_gon_req_rx_count = 0;
 #endif /* WL_CFG80211_GON_COLLISION */
+#ifdef WL_SCHED_SCAN
+	if (wl->sched_scan_req) {
+		wl_cfg80211_sched_scan_stop(wiphy, wl_to_prmry_ndev(wl));
+	}
+#endif
 #if defined(ESCAN_RESULT_PATCH)
 	if (sme->bssid)
 		memcpy(connect_req_bssid, sme->bssid, ETHER_ADDR_LEN);
@@ -7540,7 +7546,8 @@ fail:
 #define PNO_TIME		30
 #define PNO_REPEAT		4
 #define PNO_FREQ_EXPO_MAX	2
-int wl_cfg80211_sched_scan_start(struct wiphy *wiphy,
+static int
+wl_cfg80211_sched_scan_start(struct wiphy *wiphy,
                              struct net_device *dev,
                              struct cfg80211_sched_scan_request *request)
 {
@@ -7604,7 +7611,8 @@ int wl_cfg80211_sched_scan_start(struct wiphy *wiphy,
 	return 0;
 }
 
-int wl_cfg80211_sched_scan_stop(struct wiphy *wiphy, struct net_device *dev)
+static int
+wl_cfg80211_sched_scan_stop(struct wiphy *wiphy, struct net_device *dev)
 {
 	struct wl_priv *wl = wiphy_priv(wiphy);
 
