aa72dd1a4f3c3d526a1cd3732ccbf4611f7f91af
dorimanx <yuri@bynet.co.il>
2014-02-23 15:59:17 +0200

    Revert "msm: mdss: Identify white point in sysfs"
    This reverts commit db3ac7948b481b3aec3dc0635bde9209d658d868.

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -1276,7 +1276,7 @@ read_one(kgamma_bp, 15);
 read_one(kgamma_bn, 17);
 read_one(kgamma_1,   1);
 read_one(kgamma_3,   3);
-read_one(kgamma_w,   5);
+read_one(kgamma_5,   5);
 read_one(kgamma_20, 20);
 read_one(kgamma_22, 22);
 
@@ -1296,7 +1296,7 @@ write_one(kgamma_bp, 15);
 write_one(kgamma_bn, 17);
 write_one(kgamma_1,   1);
 write_one(kgamma_3,   3);
-write_one(kgamma_w,   5);
+write_one(kgamma_5,   5);
 write_one(kgamma_20, 20);
 write_one(kgamma_22, 22);
 
@@ -1311,7 +1311,7 @@ define_one_rw(kgamma_bp);
 define_one_rw(kgamma_bn);
 define_one_rw(kgamma_1);
 define_one_rw(kgamma_3);
-define_one_rw(kgamma_w);
+define_one_rw(kgamma_5);
 define_one_rw(kgamma_20);
 define_one_rw(kgamma_22);
 
@@ -1324,7 +1324,7 @@ static struct attribute *dsi_panel_attributes[] = {
 	&dev_attr_kgamma_bn.attr,
 	&dev_attr_kgamma_1.attr,
 	&dev_attr_kgamma_3.attr,
-	&dev_attr_kgamma_w.attr,
+	&dev_attr_kgamma_5.attr,
 	&dev_attr_kgamma_20.attr,
 	&dev_attr_kgamma_22.attr,
 	NULL
