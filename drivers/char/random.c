d1379bcc0f38664a360b02a4dd0c60cd14938c97
dorimanx <yuri@bynet.co.il>
2014-03-19 12:40:19 +0200

    random: entropy tweaks are all the rage nowadays

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -272,7 +272,7 @@
 /*
  * Configuration information
  */
-#define INPUT_POOL_WORDS 256
+#define INPUT_POOL_WORDS 128
 #define OUTPUT_POOL_WORDS 32
 #define SEC_XFER_SIZE 512
 #define EXTRACT_SIZE 10
@@ -727,6 +727,8 @@ out:
 void add_input_randomness(unsigned int type, unsigned int code,
 				 unsigned int value)
 {
+/* random: prevent add_input from doing anything */
+#if 0
 	static unsigned char last_value;
 
 	/* ignore autorepeat and the like */
@@ -737,6 +739,8 @@ void add_input_randomness(unsigned int type, unsigned int code,
 	last_value = value;
 	add_timer_randomness(&input_timer_state,
 			     (type << 4) ^ code ^ (code >> 4) ^ value);
+#endif
+	return;
 }
 EXPORT_SYMBOL_GPL(add_input_randomness);
 
@@ -1158,6 +1162,12 @@ void rand_initialize_disk(struct gendisk *disk)
 static ssize_t
 random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
+/*
+random: entropy tweaks are all the rage nowadays
+use nonblocking for all.  Read this web page:
+http://lwn.net/Articles/489734/ - WIP
+*/
+#if 0
 	ssize_t n, retval = 0, count = 0;
 
 	if (nbytes == 0)
@@ -1210,6 +1220,8 @@ random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 	}
 
 	return (count ? count : retval);
+#endif
+	return extract_entropy_user(&nonblocking_pool, buf, nbytes);
 }
 
 static ssize_t
