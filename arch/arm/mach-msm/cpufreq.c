d5b0c4f8ab8d43f50bd838b72d701b3f53f8ae7d
dorimanx <yuri@bynet.co.il>
2014-04-03 09:34:44 +0300

    Removed Useless TRACE CPU FREQUENCY SWITCH. TNX Alucard24

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -27,7 +27,6 @@
 #include <linux/cpumask.h>
 #include <linux/sched.h>
 #include <linux/suspend.h>
-#include <trace/events/power.h>
 #include <mach/socinfo.h>
 #include <mach/cpufreq.h>
 
@@ -176,12 +175,9 @@ static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq)
 
 	cpufreq_notify_transition(&freqs, CPUFREQ_PRECHANGE);
 
-	trace_cpu_frequency_switch_start(freqs.old, freqs.new, policy->cpu);
 	ret = acpuclk_set_rate(policy->cpu, new_freq, SETRATE_CPUFREQ);
-	if (!ret) {
-		trace_cpu_frequency_switch_end(policy->cpu);
+	if (!ret)
 		cpufreq_notify_transition(&freqs, CPUFREQ_POSTCHANGE);
-	}
 
 	/* Restore priority after clock ramp-up */
 	if (freqs.new > freqs.old && saved_sched_policy >= 0) {
@@ -313,9 +309,11 @@ int msm_cpufreq_set_freq_limits(uint32_t cpu, uint32_t min, uint32_t max)
 	else
 		limit->allowed_max = limit->max;
 
-	//pr_debug("%s: Limiting cpu %d min = %d, max = %d\n",
-	//		__func__, cpu,
-	//		limit->allowed_min, limit->allowed_max);
+#if 0
+	pr_debug("%s: Limiting cpu %d min = %d, max = %d\n",
+			__func__, cpu,
+			limit->allowed_min, limit->allowed_max);
+#endif
 
 	return 0;
 }
@@ -369,7 +367,6 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	}
 
 	if (cur_freq != table[index].frequency) {
-		int ret = 0;
 		ret = acpuclk_set_rate(policy->cpu, table[index].frequency,
 				SETRATE_CPUFREQ);
 		if (ret)
