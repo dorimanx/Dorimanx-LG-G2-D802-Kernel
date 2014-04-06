c29b34168ef6c5064cc2892b1ade0bb533f86045
dorimanx <yuri@bynet.co.il>
2014-04-06 10:55:11 +0300

    Add READY-KERNEL with all install scripts and SuperSu 1.94

--- READY-KERNEL/loki/loki.sh
+++ READY-KERNEL/loki/loki.sh
@@ -5,7 +5,8 @@
 #
 
 dd if=/dev/block/platform/msm_sdcc.1/by-name/aboot of=/tmp/loki/aboot.img
-/tmp/loki/loki_patch boot /tmp/loki/aboot.img /tmp/loki/boot.img /tmp/loki/boot.lok || exit 1
-/tmp/loki/loki_flash boot /tmp/loki/boot.lok || exit 1
+chmod 644 /tmp/loki/aboot.img
+/tmp/loki/loki_tool patch boot /tmp/loki/aboot.img /tmp/loki/boot.img /tmp/loki/boot.lok || exit 1
+/tmp/loki/loki_tool flash boot /tmp/loki/boot.lok || exit 1
 rm -rf /tmp/loki
 exit 0
