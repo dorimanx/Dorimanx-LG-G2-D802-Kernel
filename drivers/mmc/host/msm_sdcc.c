b02ac1a4ef375acdfb441d0d8aa880dc55dbd93f
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-04-08 01:15:08 +0100

    msm_sdcc: Fix building without BROADCOM_WIFI_RESERVED_MEM
    There's no DMA QoS support when a static buffer isn't in use
    
    Change-Id: If34a11c1f4362d08e3198558f7e0830f47447b5d

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -202,7 +202,7 @@ static void msmsdcc_pm_qos_update_latency(struct msmsdcc_host *host, int vote)
 		pm_qos_update_request(&host->pm_qos_req_dma,
 					PM_QOS_DEFAULT_VALUE);
 	/* LGE_CHANGE_S, [WiFi][hayun.kim@lge.com], 2013-06-12, dma qos control */
-	#if defined(CONFIG_BCMDHD) || defined (CONFIG_BCMDHD_MODULE)
+	#if (defined(CONFIG_BCMDHD) || defined (CONFIG_BCMDHD_MODULE)) && defined(CONFIG_BROADCOM_WIFI_RESERVED_MEM)
 	{
 		extern void bcm_wifi_req_dma_qos(int vote);
 		if (host->mmc && host->mmc->card && mmc_card_sdio(host->mmc->card)) {
