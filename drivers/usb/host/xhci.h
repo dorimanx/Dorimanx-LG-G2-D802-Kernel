cc90eaf5c6bb2eda66c7c657b44a713f3ae3f561
dorimanx <yuri@bynet.co.il>
2014-04-18 17:09:55 +0300

    patch-3.4.86-87 using patch -p1 <

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1482,6 +1482,9 @@ struct xhci_hcd {
 #define XHCI_RESET_ON_RESUME	(1 << 7)
 #define	XHCI_SW_BW_CHECKING	(1 << 8)
 #define XHCI_AMD_0x96_HOST	(1 << 9)
+#define XHCI_SLOW_SUSPEND	(1 << 17)
+#define XHCI_SPURIOUS_WAKEUP	(1 << 18)
+
 /*
  * In Synopsis DWC3 controller, PORTSC register access involves multiple clock
  * domains. When the software does a PORTSC write, handshakes are needed
