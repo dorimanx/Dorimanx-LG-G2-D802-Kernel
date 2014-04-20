b5a81c3ae2abc766499ae855ab8dc7c6c8234ce4
dorimanx <yuri@bynet.co.il>
2014-04-20 14:35:02 +0300

    Merged AOSP HACKS for Knock ON and Modem. For ROMS that need it.
    Like Flex ROMS.

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -2066,6 +2066,12 @@ static void touch_gesture_wakeup_func(struct work_struct *work_gesture_wakeup)
 	mutex_unlock(&i2c_suspend_lock);
 	mutex_unlock(&ts->irq_work_mutex);
 	TOUCH_INFO_MSG("INTERRUPT_STATUS_REG %x\n", buf);
+
+	/* AOSP HACK */
+	input_report_key(ts->input_dev, KEY_POWER, BUTTON_PRESSED);
+	input_report_key(ts->input_dev, KEY_POWER, BUTTON_RELEASED);
+	input_sync(ts->input_dev);
+
 	if( buf & 0x04 )
 		kobject_uevent_env(&lge_touch_sys_device.kobj, KOBJ_CHANGE, touch_wakeup_gesture);
 	else
@@ -5039,6 +5045,10 @@ static int touch_probe(struct i2c_client *client, const struct i2c_device_id *id
 			set_bit(ts->pdata->caps->button_name[ret], ts->input_dev->keybit);
 		}
 	}
+
+	/* AOSP HACK */
+	set_bit(EV_KEY, ts->input_dev->evbit);
+	set_bit(KEY_POWER, ts->input_dev->keybit);
 
 	input_set_abs_params(ts->input_dev, ABS_MT_POSITION_X, 0, ts->pdata->caps->x_max, 0, 0);
 	input_set_abs_params(ts->input_dev, ABS_MT_POSITION_Y, 0,
