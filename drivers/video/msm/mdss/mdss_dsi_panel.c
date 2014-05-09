25f8d2250864acfde0896ed19cc2e034aa6a3dda
Paul Reioux <reioux@gmail.com>
2014-05-09 08:50:09 -0700

    mdss_dsi_panel.c: add powersuspend hooks
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -30,6 +30,10 @@
 
 #include "mdss_dsi.h"
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 #ifdef CONFIG_LGE_LCD_TUNING
 #define TUNING_REGSIZE 400
@@ -486,6 +490,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_pannel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 #if defined(CONFIG_MACH_LGE)
@@ -546,6 +554,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 	if (ctrl->off_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds);
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_pannel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 
 	pr_debug("%s:-\n", __func__);
 #ifdef CONFIG_OLED_SUPPORT
