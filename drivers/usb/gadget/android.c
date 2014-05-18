47f6d511c0a50dcadaa051a8144316723655387d
Pranav Vashi <neobuddy89@gmail.com>
2014-05-19 00:27:36 +0530

    drivers: usb: Use Android device as USB keyboard/mouse
    Adapt to 3.4.y 8974/hammerhead
    Base project: https://github.com/pelya/android-keyboard-gadget
    Required app: https://play.google.com/store/apps/details?id=remote.hid.keyboard.client
    
    Steps:
    * Start ADB Debugging
    * Start app
    * Connect device to system via USB cable
    
    All credits to @pelya for this sweet hack.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -88,6 +88,9 @@
 #include "f_lg_mtp.c"
 #endif //                                       
 #include "f_accessory.c"
+#include "f_hid.h"
+#include "f_hid_android_keyboard.c"
+#include "f_hid_android_mouse.c"
 #define USB_ETH_RNDIS y
 #include "f_rndis.c"
 #include "rndis.c"
@@ -2479,6 +2482,41 @@ static struct android_usb_function uasp_function = {
 	.bind_config	= uasp_function_bind_config,
 };
 
+static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
+{
+	return ghid_setup(cdev->gadget, 2);
+}
+
+static void hid_function_cleanup(struct android_usb_function *f)
+{
+	ghid_cleanup();
+}
+
+static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
+{
+	int ret;
+	printk(KERN_INFO "hid keyboard\n");
+	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
+		return ret;
+	}
+	printk(KERN_INFO "hid mouse\n");
+	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
+		return ret;
+	}
+	return 0;
+}
+
+static struct android_usb_function hid_function = {
+	.name		= "hid",
+	.init		= hid_function_init,
+	.cleanup	= hid_function_cleanup,
+	.bind_config	= hid_function_bind_config,
+};
+
 static struct android_usb_function *supported_functions[] = {
 #ifndef CONFIG_USB_G_LGE_ANDROID_NCM
 	&mbim_function,
@@ -2520,6 +2558,7 @@ static struct android_usb_function *supported_functions[] = {
 	&audio_source_function,
 #endif
 	&uasp_function,
+	&hid_function,
 	NULL
 };
 
@@ -2799,6 +2838,7 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 	char *name;
 	char buf[256], *b;
 	int err;
+	int hid_enabled;
 
 	mutex_lock(&dev->mutex);
 
@@ -2861,8 +2901,13 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 				if (err)
 					pr_err("android_usb: Cannot enable %s",
 						name);
+				if (!strcmp(name, "hid"))
+					hid_enabled = 1;
 			}
 		}
+		/* HID driver always enabled, it's the whole point of this kernel patch */
+		if (hid_enabled)
+			android_enable_function(dev, conf, "hid");
 	}
 
 	/* Free uneeded configurations if exists */
