3a7b0b0982cf9d1a8a204d4f308b9cfa2f8fb34c
dorimanx <yuri@bynet.co.il>
2014-05-26 10:23:06 +0300

    cpufreq: Calculate load_at_max_freq based on current policy->max
    fix by myfluxi!
    
    https://github.com/myfluxi/kernel_msm/commit/4c5d76153015760f2cddedbc5f4751ccee668f28

--- drivers/cpufreq/cpufreq_HYPER.c
+++ drivers/cpufreq/cpufreq_HYPER.c
@@ -714,7 +714,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 		/* calculate the scaled load across CPU */
 		load_at_max_freq += (cur_load * policy->cur) /
-					policy->cpuinfo.max_freq;
+					policy->max;
 
 		avg_load_at_max_freq += ((load_at_max_freq +
 				j_dbs_info->load_at_prev_sample) / 2);
