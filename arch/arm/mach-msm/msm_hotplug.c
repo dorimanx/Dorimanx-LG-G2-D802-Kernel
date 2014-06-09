78bfd67d6d725d4ff22c4440adddebbed5e3c83b
Pranav Vashi <neobuddy89@gmail.com>
2014-06-09 16:37:19 +0530

    msm: hotplug: Fix broken down_lock logic
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/mach-msm/msm_hotplug.c
+++ arch/arm/mach-msm/msm_hotplug.c
@@ -388,8 +388,11 @@ static void cpu_down_work(struct work_struct *work)
 	for_each_online_cpu(cpu) {
 		if (cpu == 0)
 			continue;
-		lowest_cpu = get_lowest_load_cpu();
 		if (check_down_lock(cpu))
+			break;
+		lowest_cpu = get_lowest_load_cpu();
+		if (lowest_cpu < stats.min_cpus || lowest_cpu > stats.total_cpus
+		     || check_down_lock(lowest_cpu))
 			break;
 		cpu_down(lowest_cpu);
 		if (target >= num_online_cpus())
