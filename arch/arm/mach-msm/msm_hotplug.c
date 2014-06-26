8ec4437a0ed1c648c03179246dc30669e603c0ca
Dorimanx <yuri@bynet.co.il>
2014-06-26 20:41:39 +0300

    MSM HOTPLUG: Cleanups. Thanks NEO :)

--- arch/arm/mach-msm/msm_hotplug.c
+++ arch/arm/mach-msm/msm_hotplug.c
@@ -168,42 +168,6 @@ static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
 
 static bool io_is_busy;
 
-#if 0 /* functions moved to drivers/cpufreq/cpufreq.c */
-static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
-{
-	u64 idle_time;
-	u64 cur_wall_time;
-	u64 busy_time;
-
-	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
-
-	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
-
-	idle_time = cur_wall_time - busy_time;
-	if (wall)
-		*wall = jiffies_to_usecs(cur_wall_time);
-
-	return jiffies_to_usecs(idle_time);
-}
-
-static inline u64 get_cpu_idle_time(unsigned int cpu, u64 *wall)
-{
-	u64 idle_time = get_cpu_idle_time_us(cpu, io_is_busy ? wall : NULL);
-
-	if (idle_time == -1ULL)
-		return get_cpu_idle_time_jiffy(cpu, wall);
-	else if (!io_is_busy)
-		idle_time += get_cpu_iowait_time_us(cpu, wall);
-
-	return idle_time;
-}
-#endif
-
 static int update_average_load(unsigned int cpu)
 {
 	int ret;
@@ -550,7 +514,7 @@ reschedule:
 	defined(CONFIG_HAS_EARLYSUSPEND)
 static void msm_hotplug_suspend(struct work_struct *work)
 {
-	unsigned int cpu, max_freq = 0;
+	int cpu;
 
 	if (!hotplug.msm_enabled)
 		return;
@@ -581,8 +545,7 @@ static void msm_hotplug_suspend(struct work_struct *work)
 
 static void __ref msm_hotplug_resume(struct work_struct *work)
 {
-	int required_reschedule = 0;
-	unsigned int cpu;
+	int cpu, required_reschedule = 0;
 
 	if (!hotplug.msm_enabled)
 		return;
@@ -642,9 +605,6 @@ static void __msm_hotplug_resume(struct early_suspend *handler)
 static int lcd_notifier_callback(struct notifier_block *nb,
                                  unsigned long event, void *data)
 {
-	if (!hotplug.msm_enabled)
-		return NOTIFY_OK;
-
 	switch (event) {
 	case LCD_EVENT_ON_END:
 	case LCD_EVENT_OFF_START:
@@ -727,9 +687,9 @@ static int hotplug_input_connect(struct input_handler *handler,
 		goto err_open;
 
 	return 0;
-err_register:
-	input_unregister_handle(handle);
 err_open:
+	input_unregister_handle(handle);
+err_register:
 	kfree(handle);
 	return err;
 }
@@ -794,12 +754,11 @@ static int __ref msm_hotplug_start(void)
 #elif defined(CONFIG_HAS_EARLYSUSPEND)
 	register_early_suspend(&msm_hotplug_early_suspend_driver);
 #endif
-
 	ret = input_register_handler(&hotplug_input_handler);
 	if (ret) {
 		pr_err("%s: Failed to register input handler: %d\n",
 		       MSM_HOTPLUG, ret);
-		goto err_input;
+		goto err_dev;
 	}
 
 	stats.load_hist = kmalloc(sizeof(stats.hist_size), GFP_KERNEL);
@@ -843,10 +802,6 @@ static int __ref msm_hotplug_start(void)
 							START_DELAY);
 
 	return ret;
-
-err_input:
-	input_unregister_handler(&hotplug_input_handler);
-	destroy_workqueue(hotplug_wq);
 err_dev:
 	destroy_workqueue(hotplug_wq);
 err_out:
