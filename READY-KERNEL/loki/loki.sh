412a28eb2f577a3ec3f048c36621ec7d34379add
Dorimanx <yuri@bynet.co.il>
2014-07-03 10:36:19 +0300

    Thanks to @dr87 we now have full support for LCD panel types! LGD + JDI
    Automatically detect and set correct panel cmdline
    
    This script will find the value given by the kernel with get_panel_maker_id
    
    	LCD_RENESAS_LGD = 0
    	LCD_RENESAS_JDI = 1
    
    The lcd_maker_id value is generated from the panel hardware, is always accurate and will always set the correct cmdline of your panel
    
    This script can also simply be copy pasted for a kernel zip in loki.sh before the loki function.
    It has no other requirements besides the script itself since it directly searches and modifies the boot.img
    and doesn't require the boot.img to be unpacked.
    
    Do not shorten the search function as we need to ensure only the cmdline is being changed and nothing in the actual kernel source.
    
    Continue building the kernel with either the full lgd or jdi cmdline, the script will change the panel as needed.
    
    Currently the script looks for the extracted boot.img in /tmp and /tmp/loki
    
    Signed-off-by: dr87 <nb00870@gmail.com>
    
    https://github.com/dr87/4.4_g2-common/commit/58feacbc7ba19c4aa63612037dcc038b4f8b2dde

--- READY-KERNEL/loki/loki.sh
+++ READY-KERNEL/loki/loki.sh
@@ -4,6 +4,30 @@
 # See here for more information on loki: https://github.com/djrbliss/loki
 #
 
+# 4.4 Kernel - Panel Detection - by @dr87 / dr87@xda
+#
+# Detect panel and swap as necessary
+# lcd_maker_id is determined by get_panel_maker_id on the hardware and is always accurate
+# This searches directly in the boot.img and has no other requirements
+# Do not shorten the search or you may change the actual kernel source
+#
+#	LCD_RENESAS_LGD = 0
+#	LCD_RENESAS_JDI = 1
+# Dorimanx Note:
+# My kernel compiled for LGD panel by default! so it's will be changed to JDI panel only when needed.
+
+# Search for boot.img in /tmp/loki
+
+lcdmaker=$(grep -c "lcd_maker_id=1" /proc/cmdline)
+if [ "$lcdmaker" -eq "1" ]; then
+	echo "JDI panel detected";
+	find /tmp/loki/boot.img -type f -exec sed -i 's/console=ttyHSL0,115200,n8 androidboot.hardware=g2 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/console=ttyHSL0,115200,n8 androidboot.hardware=g2 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/g' {} \;
+	echo "JDI panel set in kernel config!";
+else
+	echo "LGD panel detected and set in config";
+fi;
+
+# Loki
 dd if=/dev/block/platform/msm_sdcc.1/by-name/aboot of=/tmp/loki/aboot.img
 chmod 644 /tmp/loki/aboot.img
 /tmp/loki/loki_tool patch boot /tmp/loki/aboot.img /tmp/loki/boot.img /tmp/loki/boot.lok || exit 1
