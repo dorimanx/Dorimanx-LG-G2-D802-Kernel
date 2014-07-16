ef2afd6770f18a5766149485b3cc887635a419ed
Dorimanx <yuri@bynet.co.il>
2014-07-16 17:02:49 +0300

    THERMAL CONTROL: Prevent too high CPU temp on boot.

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -62,11 +62,11 @@ static struct msm_thermal_data msm_thermal_info;
 static struct msm_thermal_data msm_thermal_info_local = {
 	.sensor_id = 5,
 	.poll_ms = DEFAULT_POLLING_MS,
-	.limit_temp_degC = 82,
+	.limit_temp_degC = 77,
 	.temp_hysteresis_degC = 5,
 	.freq_step = 2,
 	.freq_control_mask = 0xf,
-	.core_limit_temp_degC = 82,
+	.core_limit_temp_degC = 80,
 	.core_temp_hysteresis_degC = 5,
 	.core_control_mask = 0xe,
 };
