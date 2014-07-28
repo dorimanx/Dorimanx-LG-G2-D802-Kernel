d1fae215e5455393c579cdf3cc45daf095bd91f6
Dorimanx <yuri@bynet.co.il>
2014-07-28 19:25:22 +0300

    THERMAL CONTROL: Use sensor 0 (GLOBAL CPU TEMP) and tune boot values.

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -60,7 +60,7 @@ static uint32_t hist_index = 0;
 static struct msm_thermal_data msm_thermal_info;
 
 static struct msm_thermal_data msm_thermal_info_local = {
-	.sensor_id = 5,
+	.sensor_id = 0,
 	.poll_ms = DEFAULT_POLLING_MS,
 	.limit_temp_degC = 77,
 	.temp_hysteresis_degC = 5,
