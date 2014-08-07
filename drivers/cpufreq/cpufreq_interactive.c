4aa259ffe7717397c610456e1d1a13403ba68dae
Dorimanx <yuri@bynet.co.il>
2014-08-07 13:30:28 +0300

    Interactive: Code fixed to adapt to my branch.

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -195,11 +195,10 @@ static void cpufreq_interactive_timer_resched(
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
 		get_cpu_idle_time(smp_processor_id(),
-				     &pcpu->time_in_idle_timestamp,
-					io_is_busy);
+				     &pcpu->time_in_idle_timestamp, io_is_busy);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
-	expires = jiffies + usecs_to_jiffies(pcpu->usecs);
+	expires = jiffies + usecs_to_jiffies(usecs);
 	mod_timer_pinned(&pcpu->cpu_timer, expires);
 
 	if (pcpu->timer_slack_val >= 0 &&
@@ -238,8 +237,7 @@ static void cpufreq_interactive_timer_start(int cpu)
 
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
-		get_cpu_idle_time(cpu, &pcpu->time_in_idle_timestamp,
-				io_is_busy);
+		get_cpu_idle_time(cpu, &pcpu->time_in_idle_timestamp, io_is_busy);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
 	spin_unlock_irqrestore(&pcpu->load_lock, flags);
@@ -468,7 +466,7 @@ static unsigned
 get_timer_rate(struct cpufreq_interactive_cpuinfo *pcpu, unsigned int cpu_load)
 {
 	if (cpu_load < check_fast_load || pcpu->target_freq == pcpu->policy->max)
-		return timer_rate;
+		return pcpu->timer_rate;
 	else
 		return check_fast_timer_rate;
 }
@@ -693,7 +691,7 @@ static void cpufreq_interactive_idle_start(void)
 		 * the CPUFreq driver.
 		 */
 		if (!pending) {
-			cpufreq_interactive_timer_resched(pcpu, timer_rate);
+			cpufreq_interactive_timer_resched(pcpu, pcpu->timer_rate);
 
 			if ((pcpu->policy->cur == pcpu->policy->max) &&
 				(now - pcpu->hispeed_validate_time) >
@@ -721,7 +719,7 @@ static void cpufreq_interactive_idle_end(void)
 
 	/* Arm the timer for 1-2 ticks later if not already. */
 	if (!timer_pending(&pcpu->cpu_timer)) {
-		cpufreq_interactive_timer_resched(pcpu, timer_rate);
+		cpufreq_interactive_timer_resched(pcpu, pcpu->timer_rate);
 	} else if (time_after_eq(jiffies, pcpu->cpu_timer.expires)) {
 		del_timer(&pcpu->cpu_timer);
 		del_timer(&pcpu->cpu_slack_timer);
