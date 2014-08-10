a1f99f3b6dad71210eb7568bf3eefe563fc252bf
myfluxi <linflux@arcor.de>
2014-08-10 17:08:51 +0200

    ASoC: wcd9320: Add NULL checks
    An failed device start is bad enough, don't crash the device.
    Non-negative errors look a bit funny, though.

--- sound/soc/codecs/wcd9320.c
+++ sound/soc/codecs/wcd9320.c
@@ -4612,7 +4612,12 @@ unsigned int taiko_read(struct snd_soc_codec *codec,
 {
 	unsigned int val;
 	int ret;
-	struct wcd9xxx *wcd9xxx = codec->control_data;
+	struct wcd9xxx *wcd9xxx;
+
+	if (!codec)
+		return 0;
+
+	wcd9xxx = codec->control_data;
 
 	if (reg == SND_SOC_NOPM)
 		return 0;
@@ -4646,8 +4651,12 @@ int taiko_write(struct snd_soc_codec *codec, unsigned int reg,
 #ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
 	int val;
 #endif
+	struct wcd9xxx *wcd9xxx;
 
-	struct wcd9xxx *wcd9xxx = codec->control_data;
+	if (!codec)
+		return 0;
+
+	wcd9xxx = codec->control_data;
 
 	if (reg == SND_SOC_NOPM)
 		return 0;
