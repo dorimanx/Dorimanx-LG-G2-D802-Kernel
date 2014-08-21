20ba44f38fd85520edbc6f4b288ef63beee3bffc
Dorimanx <yuri@bynet.co.il>
2014-08-21 15:45:46 +0300

    Clean dual int vm_iomap_memory in include/linux/mm.h

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1535,8 +1535,6 @@ int remap_pfn_range(struct vm_area_struct *, unsigned long addr,
 int vm_insert_page(struct vm_area_struct *, unsigned long addr, struct page *);
 int vm_insert_pfn(struct vm_area_struct *vma, unsigned long addr,
 			unsigned long pfn);
-int vm_iomap_memory(struct vm_area_struct *vma, phys_addr_t start, unsigned long len);
-
 int vm_insert_mixed(struct vm_area_struct *vma, unsigned long addr,
 			unsigned long pfn);
 int vm_iomap_memory(struct vm_area_struct *vma, phys_addr_t start, unsigned long len);
