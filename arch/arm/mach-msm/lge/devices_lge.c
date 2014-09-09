ad82a653bb281de9503d84ff1d64655a9ffd59f2
Dorimanx <yuri@bynet.co.il>
2014-09-09 17:34:15 +0300

    Merge Kexec Hardboot (Multy Kernel Boot code) Thanks to @PatrikKT
    This is backported and modded from HTC for LG G2.
    
    https://gist.github.com/PatrikKT/50faf32e8931d51c0c9a

--- arch/arm/mach-msm/lge/devices_lge.c
+++ arch/arm/mach-msm/lge/devices_lge.c
@@ -40,6 +40,10 @@
 /* in drivers/staging/android */
 #include "ram_console.h"
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <linux/memblock.h>
+#endif
+
 static int cn_arr_len = 3;
 /*                                                              */
 #ifdef CONFIG_LGE_PM
@@ -226,6 +230,29 @@ void __init lge_add_persist_ram_devices(void)
 
 void __init lge_reserve(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	/*
+	 * Reserve space for hardboot page - just after ram_console,
+	 * at the start of second memory bank
+	 */
+	int ret;
+	phys_addr_t start;
+	struct membank* bank;
+
+	if (meminfo.nr_banks < 2) {
+		pr_err("%s: not enough membank\n", __func__);
+		return;
+	}
+
+	bank = &meminfo.bank[1];
+	start = bank->start + SZ_1M + LGE_PERSISTENT_RAM_SIZE;
+	ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
+
 #if defined(CONFIG_ANDROID_PERSISTENT_RAM)
 	lge_add_persist_ram_devices();
 #endif
