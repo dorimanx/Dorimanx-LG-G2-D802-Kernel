525e8c0bac1a07236ef52ab8ced08ca341b7614f
Dorimanx <yuri@bynet.co.il>
2014-10-09 22:03:41 +0300

    MSM THERMAL: set default max cpu temp to 75c and core to 74c

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -62,12 +62,12 @@ static struct msm_thermal_data msm_thermal_info;
 static struct msm_thermal_data_intelli msm_thermal_info_local = {
 	.sensor_id = 0,
 	.poll_ms = DEFAULT_POLLING_MS,
-	.limit_temp_degC = 77,
+	.limit_temp_degC = 75,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
 	.freq_control_mask = 0xf,
-	.core_limit_temp_degC = 79,
-	.core_temp_hysteresis_degC = 5,
+	.core_limit_temp_degC = 74,
+	.core_temp_hysteresis_degC = 10,
 	.core_control_mask = 0xe,
 };
 
