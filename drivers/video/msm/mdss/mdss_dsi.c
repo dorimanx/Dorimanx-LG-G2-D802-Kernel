543fc72c81e6c15cdcd122597eb75bebeb5bfe76
Dorimanx <yuri@bynet.co.il>
2014-10-10 22:24:28 +0300

    POWERSUSPEND: revert to v1.5 remove MDSS hook, i dont use that.

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -24,9 +24,6 @@
 #ifdef CONFIG_MACH_LGE
 #include <linux/lcd_notify.h>
 #endif
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#endif
 
 #include "mdss.h"
 #include "mdss_panel.h"
@@ -1031,11 +1028,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
 #endif
 
-#ifdef CONFIG_POWERSUSPEND
-		if (suspend_mode == 2)
-			set_power_suspend_state_panel_hook(
-				POWER_SUSPEND_INACTIVE);
-#endif
 		rc = mdss_dsi_on(pdata);
 		mdss_dsi_op_mode_config(pdata->panel_info.mipi.mode,
 							pdata);
@@ -1066,11 +1058,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 #endif
 
-#ifdef CONFIG_POWERSUSPEND
-		if (suspend_mode == 2)
-			set_power_suspend_state_panel_hook(
-				POWER_SUSPEND_ACTIVE);
-#endif
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		ctrl_pdata->ctrl_state &= ~CTRL_STATE_MDP_ACTIVE;
