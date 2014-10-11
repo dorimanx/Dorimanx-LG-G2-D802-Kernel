dbd0a95c97635bf8a035394f9e048dc1526515b3
Dorimanx <yuri@bynet.co.il>
2014-10-11 18:16:45 +0300

    Kernel Install script: set simple perms for /etc/ files. rom will correct.
    *This also allow old recovery's to install kernel and not mess the files.

---
+++ READY-KERNEL/loki/permfix.sh
@@ -0,0 +1,7 @@
+#!/sbin/sh
+
+chmod 755 /system/etc/thermal-engine-8974.conf
+chmod 755 /system/etc/thermal-engine-8974-default.conf
+chmod 755 /system/etc/thermal-engine-8974-ftm.conf
+chmod 755 /system/etc/audio_policy.conf
+
