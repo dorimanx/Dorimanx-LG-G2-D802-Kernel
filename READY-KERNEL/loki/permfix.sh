4be7e01baaa56261a93f225af0f4b3677fad6dc4
Dorimanx <yuri@bynet.co.il>
2014-10-14 01:08:09 +0300

    Fixed Kernel install script and set smart perms again.

--- READY-KERNEL/loki/permfix.sh
+++
@@ -1,8 +0,0 @@
-#!/sbin/sh
-
-chmod 755 /system/etc/thermal-engine-8974.conf
-chmod 755 /system/etc/thermal-engine-8974-default.conf
-chmod 755 /system/etc/thermal-engine-8974-ftm.conf
-chmod 755 /system/etc/audio_policy.conf
-chmod 755 /system/xbin/sqlite3
-
