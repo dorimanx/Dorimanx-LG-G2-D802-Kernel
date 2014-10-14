154e2f4255504062e0c510f55ab87c98b7d8817b
Dorimanx <yuri@bynet.co.il>
2014-10-14 03:01:29 +0300

    New Touch Boost driver by @Alucard24 and New CPUFREQ_LIMIT V4.0
    *Replaced old cpu-boost driver with new fast and great from Alucard24.
    New driver is using new freq limits functions in main cpufreq driver.
    working super fast, locking on all cores, and can be tuned to boost
    on 1->2->3->all, no useless shit that exist in cpu-boost.
    
    *Alucard helped me to create new LG needed function for ROM thermal
    binary. after thermal throttle is over, and core is cold,
    ROM binary cant restore max freq on all cores, so they stay no maxed
    and before the thermal event.
    now it's used to detect max freq set on core 0 when it's limited less.
    then all cores set to there limit max set before.
    ROM binary restore max freq only to core0 or active cores.
    Kernel will help and restore all as should be.
    
    *Cleaned MSM and Intelli Hotplugs from net needed var.
    *Merged from alucard all that needed for new cpu booster code.
    
    Big thanks to @Alucard24 for suprime help and close work on this project!

--- arch/arm/mach-msm/intelli_plug.c
+++ arch/arm/mach-msm/intelli_plug.c
@@ -349,11 +349,7 @@ static void __ref intelli_plug_resume(struct work_struct *work)
 		dprintk("%s: resumed.\n", INTELLI_PLUG);
 	}
 
-#if defined(CONFIG_LCD_NOTIFY) || defined(CONFIG_MACH_LGE)
-	if (wakeup_boost || required_wakeup) {
-#else
 	if (required_wakeup) {
-#endif
 		/* Fire up all CPUs */
 		for_each_cpu_not(cpu, cpu_online_mask) {
 			if (cpu == 0)
@@ -397,26 +393,21 @@ static void __ref __intelli_plug_resume(struct power_suspend *handler)
 static void __ref __intelli_plug_resume(struct early_suspend *handler)
 #endif
 {
-#if defined(CONFIG_LCD_NOTIFY) || defined(CONFIG_MACH_LGE)
 	int cpu;
-#endif
 
 	if (atomic_read(&intelli_plug_active) == 0)
 		return;
 
 	if (!hotplug_suspend) {
-#if defined(CONFIG_LCD_NOTIFY) || defined(CONFIG_MACH_LGE)
-		if (wakeup_boost) {
-			/* Fire up all CPUs */
-			for_each_cpu_not(cpu, cpu_online_mask) {
-				if (cpu == 0)
-					continue;
-				cpu_up(cpu);
-				apply_down_lock(cpu);
-			}
-			dprintk("%s: wakeup boosted.\n", INTELLI_PLUG);
+		/* Fire up all CPUs */
+		for_each_cpu_not(cpu, cpu_online_mask) {
+			if (cpu == 0)
+				continue;
+			cpu_up(cpu);
+			apply_down_lock(cpu);
 		}
-#endif
+		dprintk("%s: wakeup boosted.\n", INTELLI_PLUG);
+
 		return;
 	}
 
