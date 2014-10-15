738976a61184bb1b5c2c879693fc177d2611bcf7
Dorimanx <yuri@bynet.co.il>
2014-10-15 18:53:19 +0300

    CPUFREQ LIMIT: remove no longer need code from store min/max functions.
    *We have 2 functions now, one for max + limit and 1 for min freq.
    So need to clean old checks and sets.
    
    This fix bug with min freq change by ROM thermal on thermal event.
    
    Thanks to @Alucard24 for help and support.

--- arch/arm/mach-msm/cpufreq_limit.c
+++ arch/arm/mach-msm/cpufreq_limit.c
@@ -32,7 +32,7 @@
 #include <mach/cpufreq.h>
 
 #define MSM_CPUFREQ_LIMIT_MAJOR		4
-#define MSM_CPUFREQ_LIMIT_MINOR		1
+#define MSM_CPUFREQ_LIMIT_MINOR		2
 
 #define MSM_LIMIT			"msm_cpufreq_limit"
 
