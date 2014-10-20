487933fab334f4a58a70ab0ae67c93e2a420b362
myfluxi <linflux@arcor.de>
2014-10-20 21:34:16 +0200

    usb: dwc3: Fix usb online prop setting failure

--- drivers/usb/dwc3/dwc3_otg.c
+++ drivers/usb/dwc3/dwc3_otg.c
@@ -682,7 +682,7 @@ static int dwc3_otg_set_power(struct usb_phy *phy, unsigned mA)
 			dotg->psy = power_supply_get_by_name("ac");
 		}
 #endif
-	} else if (dotg->charger->max_power > 0 && (mA == 0 || mA == 2)) {
+	} else if (mA == 0 || mA == 2) {
 		/* Disable charging */
 		if (power_supply_set_online(dotg->psy, false))
 			goto psy_error;
