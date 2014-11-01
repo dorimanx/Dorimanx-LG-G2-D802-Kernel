988ba89561bab3c81e88358f363ffe4934c979a4
Dorimanx <yuri@bynet.co.il>
2014-11-01 19:31:06 +0200

    KGSL GPU OC: Fixed mess for my kernel.
    *I was getting freq 27Mhz (lvl 7) set on high temp. that lead to gpu stall.
    
    Now all OK.
    
    @neoboddy89 please check this. you too have the same amount of freqs for GPU.
    you need to corect this.!
    
    num of gpu freqs is 8! not 7 (0-7) as 7 is the 27mhz we cant really use it
    for GPU!
    so we must lock min to 6.

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -160,6 +160,7 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 
 	trace_kgsl_pwrlevel(device, pwr->active_pwrlevel, pwrlevel->gpu_freq);
 }
+
 EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
 
 static int kgsl_pwrctrl_thermal_pwrlevel_store(struct device *dev,
@@ -183,8 +184,8 @@ static int kgsl_pwrctrl_thermal_pwrlevel_store(struct device *dev,
 
 	kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
 
-	if (level > pwr->num_pwrlevels - 1)
-		level = pwr->num_pwrlevels - 1;
+	if (level > pwr->num_pwrlevels - 2)
+		level = pwr->num_pwrlevels - 2;
 
 	pwr->thermal_pwrlevel = level;
 
@@ -287,8 +288,8 @@ static int kgsl_pwrctrl_min_pwrlevel_store(struct device *dev,
 		return ret;
 
 	kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
-	if (level > pwr->num_pwrlevels - 1)
-		level = pwr->num_pwrlevels - 1;
+	if (level > pwr->num_pwrlevels - 2)
+		level = pwr->num_pwrlevels - 2;
 
 	/* You can't set a minimum power level lower than the maximum */
 	if (level < pwr->max_pwrlevel)
@@ -382,8 +383,11 @@ static int kgsl_pwrctrl_max_gpuclk_store(struct device *dev,
 
 	kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
 	level = _get_nearest_pwrlevel(pwr, val);
-	if (level < 0 || level > 7)
+	if (level < 0)
 		goto done;
+
+	if (level > 6)
+		level = 6;
 
 	pwr->thermal_pwrlevel = level - 2;
 
@@ -996,17 +1000,16 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 		result = -EINVAL;
 		goto done;
 	}
-	pwr->num_pwrlevels = pdata->num_levels;
+	pwr->num_pwrlevels = pdata->num_levels; /* 0-7 = 8 */
 
 	/* Initialize the user and thermal clock constraints */
 
-	/* Initialize with extremes else devfreq will prevent OC */
 	pwr->max_pwrlevel = 0;
-	pwr->min_pwrlevel = pdata->num_levels - 1;
+	pwr->min_pwrlevel = pdata->num_levels - 2; /* = 6 (100Mhz) */
 	pwr->thermal_pwrlevel = 0;
 
 	pwr->active_pwrlevel = pdata->init_level;
-	pwr->default_pwrlevel = pwr->min_pwrlevel;
+	pwr->default_pwrlevel = pdata->init_level; /* init is 6 (100Mhz) */
 	pwr->init_pwrlevel = pdata->init_level;
 	pwr->wakeup_maxpwrlevel = 0;
 	for (i = 0; i < pdata->num_levels; i++) {
