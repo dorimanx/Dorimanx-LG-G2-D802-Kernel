f2c9c0ff0ce72b0d1403a56ec8a0eb76c32bf16f
Dorimanx <yuri@bynet.co.il>
2014-12-07 02:42:45 +0200

    MMC: Reverting all WAKE/SUSPEND changes to fix MMC hangs during suspend.
    This reverts:
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/e439062ac344a9bd88b5560f58b6ef654e35c60a
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/f436acc570d16bf330b875fcd94b077e55051a58
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/545d315391b0613932adfd373b934767184f5e21
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/1a6c77ca40177f4ff8f6af60ab8197cd16a858c6
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/001fdb4ca1cd06ba4176567d3eec67f28f900dd1
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/0049ab570ada4b84909e43f5f89db47c01951e94
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/6241623da43deeaaef849ac451ed6b2b6d499898
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/ae8695e2fd8021f64c4748f1b0ebc872ebc1778b
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/38f752bed0ea0be8573a765e7ea84ba4c6424e3a
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/0ec4df36a63aee16f2d37cdc8ef0400d7433ba36
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/8b00c3d522e4332f789830b8181bb8f60bb810f4
    
    I need stability, i dont see any change with all that added. but i see dead MMC...
    now ok after many tests.

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -201,12 +201,6 @@ struct mmc_hotplug {
 extern int mmc_cd_get_status(struct mmc_host *host);
 #endif
 
-enum dev_state {
-	DEV_SUSPENDING = 1,
-	DEV_SUSPENDED,
-	DEV_RESUMED,
-};
-
 struct mmc_host {
 	struct device		*parent;
 	struct device		class_dev;
@@ -433,7 +427,6 @@ struct mmc_host {
 		struct delayed_work work;
 		enum mmc_load	state;
 	} clk_scaling;
-	enum dev_state dev_status;
 	unsigned long		private[0] ____cacheline_aligned;
 };
 
