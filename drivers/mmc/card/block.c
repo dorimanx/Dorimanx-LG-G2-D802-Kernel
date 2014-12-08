70d6907c24124d29f11f8c973fbd9b35fce5cd76
Dorimanx <yuri@bynet.co.il>
2014-12-08 15:32:36 +0200

    MMC: Restore MMC_BLOCK_DEFERRED_RESUME changes. for future use.
    This restore:
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/0ec4df36a63aee16f2d37cdc8ef0400d7433ba36
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/38f752bed0ea0be8573a765e7ea84ba4c6424e3a
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/ae8695e2fd8021f64c4748f1b0ebc872ebc1778b
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/6241623da43deeaaef849ac451ed6b2b6d499898
    https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel/commit/0049ab570ada4b84909e43f5f89db47c01951e94
    
    + change not delete:
    ifndef CONFIG_MMC_BLOCK_DEFERRED_RESUME
            if (host->bus_ops->detect && !host->bus_dead)
                    host->bus_ops->detect(host);
    endif

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2750,17 +2750,14 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 	struct mmc_host *host = card->host;
 	unsigned long flags;
 
-#ifdef CONFIG_MMC_BLOCK_DEFERRED_RESUME
-	if (mmc_bus_needs_resume(card->host)) {
-		mmc_resume_bus(card->host);
-		mmc_blk_set_blksize(md, card);
-	}
-#endif
-
 	if (req && !mq->mqrq_prev->req) {
 		mmc_rpm_hold(host, &card->dev);
 		/* claim host only for the first request */
 		mmc_claim_host(card->host);
+#ifdef CONFIG_MMC_BLOCK_DEFERRED_RESUME
+		if (mmc_bus_needs_resume(card->host))
+			mmc_resume_bus(card->host);
+#endif
 		if (card->ext_csd.bkops_en)
 			mmc_stop_bkops(card);
 	}
