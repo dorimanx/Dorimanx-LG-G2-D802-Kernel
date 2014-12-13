e313b656283e57552d7f654452f28185160c3d2c
Dorimanx <yuri@bynet.co.il>
2014-12-13 16:34:05 +0200

    GIT ADD auto generated policy for F01F build.

---
+++ security/ccsecurity/builtin-policy.h
@@ -0,0 +1,10 @@
+static char ccs_builtin_profile[] __initdata =
+"";
+static char ccs_builtin_exception_policy[] __initdata =
+"";
+static char ccs_builtin_domain_policy[] __initdata =
+"";
+static char ccs_builtin_manager[] __initdata =
+"";
+static char ccs_builtin_stat[] __initdata =
+"";
