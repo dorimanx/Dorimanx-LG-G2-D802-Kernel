300774b7f3ec0fb1d859556e99d827e346aef4bc
Dorimanx <yuri@bynet.co.il>
2015-01-25 17:20:37 +0200

    CPUFREQ: Add YankActive CPU GOV as is. by user request.
    https://github.com/friedrich420/S4-TW-AEL-Kernel-v4Plus/blob/master/drivers/cpufreq/cpufreq_yankactive.c

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -427,6 +427,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
 #endif
 
 
