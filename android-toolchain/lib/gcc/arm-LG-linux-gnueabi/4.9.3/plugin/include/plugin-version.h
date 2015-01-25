108ce00d752ee7411a92836588b043b51a31cda2
Dorimanx <yuri@bynet.co.il>
2015-01-25 19:20:56 +0200

    LINARO 2015.01 Kernel compiler update from source. for LG.

--- android-toolchain/lib/gcc/arm-LG-linux-gnueabi/4.9.3/plugin/include/plugin-version.h
+++ android-toolchain/lib/gcc/arm-LG-linux-gnueabi/4.9.3/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "4.9.3";
-static char datestamp[] = "20141205";
+static char datestamp[] = "20150113";
 static char devphase[] = "prerelease";
 static char revision[] = "";
 
