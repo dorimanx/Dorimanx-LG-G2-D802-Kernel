e58bfccee431aafdcbe71f5777a41f50ac15705c
Pranav Vashi <neobuddy89@gmail.com>
2015-02-28 02:01:17 +0530

    msm: bricked_hotplug: Use MDSS notifiers
    * Instead of LCD notifer.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/mach-msm/bricked_hotplug.c
+++ arch/arm/mach-msm/bricked_hotplug.c
@@ -11,7 +11,6 @@
  *
  */
 
-#include <linux/lcd_notify.h>
 #include <linux/init.h>
 #include <linux/cpufreq.h>
 #include <linux/workqueue.h>
@@ -24,6 +23,8 @@
 #include <linux/platform_device.h>
 #include <linux/module.h>
 #include <linux/device.h>
+#include <linux/fb.h>
+#include <linux/notifier.h>
 
 #define DEBUG 0
 
@@ -326,46 +327,46 @@ static void __ref bricked_hotplug_resume(struct work_struct *work)
 	}
 }
 
-static int lcd_notifier_callback(struct notifier_block *this,
-				unsigned long event, void *data) {
-
-	if (!hotplug.bricked_enabled)
-		return MSM_MPDEC_DISABLED;
+static int fb_notifier_callback(struct notifier_block *self,
+				unsigned long event, void *data)
+{
+	struct fb_event *evdata = data;
+	int *blank;
 
 	if (!hotplug.hotplug_suspend)
 		return NOTIFY_OK;
 
-	switch (event) {
-	case LCD_EVENT_ON_END:
-	case LCD_EVENT_OFF_START:
-		break;
-	case LCD_EVENT_ON_START:
-		flush_workqueue(susp_wq);
-		cancel_delayed_work_sync(&suspend_work);
-		queue_work_on(0, susp_wq, &resume_work);
-		break;
-	case LCD_EVENT_OFF_END:
-		INIT_DELAYED_WORK(&suspend_work, bricked_hotplug_suspend);
-		queue_delayed_work_on(0, susp_wq, &suspend_work, 
-				 msecs_to_jiffies(hotplug.suspend_defer_time * 1000)); 
-		break;
-	default:
-		break;
+	if (evdata && evdata->data && event == FB_EVENT_BLANK) {
+		blank = evdata->data;
+		switch (*blank) {
+			case FB_BLANK_UNBLANK:
+				//display on
+				flush_workqueue(susp_wq);
+				cancel_delayed_work_sync(&suspend_work);
+				queue_work_on(0, susp_wq, &resume_work);
+				break;
+			case FB_BLANK_POWERDOWN:
+			case FB_BLANK_HSYNC_SUSPEND:
+			case FB_BLANK_VSYNC_SUSPEND:
+			case FB_BLANK_NORMAL:
+				//display off
+				INIT_DELAYED_WORK(&suspend_work, bricked_hotplug_suspend);
+				queue_delayed_work_on(0, susp_wq, &suspend_work, 
+					msecs_to_jiffies(hotplug.suspend_defer_time * 1000)); 
+				break;
+		}
 	}
 
 	return NOTIFY_OK;
 }
+
 
 static int bricked_hotplug_start(void)
 {
 	int cpu, ret = 0;
 	struct down_lock *dl;
 
-	hotplug_wq = alloc_workqueue(
-						"bricked_hotplug_wq",
-						WQ_UNBOUND | WQ_RESCUER | WQ_FREEZABLE,
-						1
-						);
+	hotplug_wq = alloc_workqueue("bricked_hotplug", WQ_HIGHPRI | WQ_FREEZABLE, 0);
 	if (!hotplug_wq) {
 		ret = -ENOMEM;
 		goto err_out;
@@ -380,12 +381,7 @@ static int bricked_hotplug_start(void)
 		goto err_out;
 	}
 
-	notif.notifier_call = lcd_notifier_callback;
-	if (lcd_register_client(&notif) != 0) {
-		pr_err("%s: Failed to register lcd callback\n", __func__);
-		ret = -EINVAL;
-		goto err_dev;
-	}
+	notif.notifier_call = fb_notifier_callback;
 
 	mutex_init(&hotplug.bricked_cpu_mutex);
 	mutex_init(&hotplug.bricked_hotplug_mutex);
@@ -404,8 +400,6 @@ static int bricked_hotplug_start(void)
 					msecs_to_jiffies(hotplug.startdelay));
 
 	return ret;
-err_dev:
-	destroy_workqueue(hotplug_wq);
 err_out:
 	hotplug.bricked_enabled = 0;
 	return ret;
@@ -427,7 +421,7 @@ static void bricked_hotplug_stop(void)
 	cancel_delayed_work_sync(&hotplug_work);
 	mutex_destroy(&hotplug.bricked_hotplug_mutex);
 	mutex_destroy(&hotplug.bricked_cpu_mutex);
-	lcd_unregister_client(&notif);
+	notif.notifier_call = NULL;
 	destroy_workqueue(susp_wq);
 	destroy_workqueue(hotplug_wq);
 
@@ -786,7 +780,7 @@ static struct attribute_group attr_group = {
 
 /**************************** SYSFS END ****************************/
 
-static int __devinit bricked_hotplug_probe(struct platform_device *pdev)
+static int bricked_hotplug_probe(struct platform_device *pdev)
 {
 	int ret = 0;
 	struct kobject *bricked_kobj;
