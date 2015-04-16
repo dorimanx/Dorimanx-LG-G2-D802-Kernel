e42c8b8443f49aff4ca71d99f1964dcb39627777
Dorimanx <yuri@bynet.co.il>
2015-04-16 16:37:36 +0300

    When i was updating LP source with kernel patches! found missing code!
    I have merged that commit!
    https://github.com/torvalds/linux/commit/36caff5d795429c572443894e8789c2150dd796b
    
    but did it HALF WAY!!! seems that i was too tired! or dumm... :)
    
    any way fix this now!

--- drivers/usb/core/message.c
+++ drivers/usb/core/message.c
@@ -1757,45 +1757,8 @@ free_interfaces:
 	/* Get rid of pending async Set-Config requests for this device */
 	cancel_async_set_config(dev);
 
-	/* Make sure we have bandwidth (and available HCD resources) for this
-	 * configuration.  Remove endpoints from the schedule if we're dropping
-	 * this configuration to set configuration 0.  After this point, the
-	 * host controller will not allow submissions to dropped endpoints.  If
-	 * this call fails, the device state is unchanged.
-	 */
-	mutex_lock(hcd->bandwidth_mutex);
-	ret = usb_hcd_alloc_bandwidth(dev, cp, NULL, NULL);
-	if (ret < 0) {
-		mutex_unlock(hcd->bandwidth_mutex);
-		usb_autosuspend_device(dev);
-		goto free_interfaces;
-	}
-
-	dev->actconfig = cp;
-	if (cp)
-		usb_notify_config_device(dev);
-	ret = usb_control_msg(dev, usb_sndctrlpipe(dev, 0),
-			      USB_REQ_SET_CONFIGURATION, 0, configuration, 0,
-			      NULL, 0, USB_CTRL_SET_TIMEOUT);
-	if (ret < 0) {
-		/* All the old state is gone, so what else can we do?
-		 * The device is probably useless now anyway.
-		 */
-		dev->actconfig = cp = NULL;
-	}
-
-	if (!cp) {
-		usb_notify_config_device(dev);
-		usb_set_device_state(dev, USB_STATE_ADDRESS);
-		usb_hcd_alloc_bandwidth(dev, NULL, NULL, NULL);
-		mutex_unlock(hcd->bandwidth_mutex);
-		usb_autosuspend_device(dev);
-		goto free_interfaces;
-	}
-	mutex_unlock(hcd->bandwidth_mutex);
-	usb_set_device_state(dev, USB_STATE_CONFIGURED);
-
-	/* Initialize the new interface structures and the
+	/*
+	 * Initialize the new interface structures and the
 	 * hc/hcd/usbcore interface/endpoint state.
 	 */
 	for (i = 0; i < nintf; ++i) {
@@ -1857,9 +1820,12 @@ free_interfaces:
 	}
 
 	dev->actconfig = cp;
+	if (cp)
+		usb_notify_config_device(dev);
 	mutex_unlock(hcd->bandwidth_mutex);
 
 	if (!cp) {
+		usb_notify_config_device(dev);
 		usb_set_device_state(dev, USB_STATE_ADDRESS);
 
 		/* Leave LPM disabled while the device is unconfigured. */
