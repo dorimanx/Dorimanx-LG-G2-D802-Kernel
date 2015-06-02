bf8f8d78635dfc0dd60a412cf23861fab6dc7a14
Dorimanx <yuri@bynet.co.il>
2015-06-02 12:40:31 +0300

    READY BUILD: add dorimanx app and color control to zip install. + cleanup.
    Also add F2FS latest tools.

--- READY-KERNEL/panelfix.sh
+++ READY-KERNEL/panelfix.sh
@@ -23,5 +23,16 @@ else
 	echo "LGD panel detected and set in config";
 fi;
 
+
+if [ -e /system/app/STweaks.apk ]; then
+	rm /system/app/STweaks.apk;
+fi;
+if [ -e /system/priv-app/STweaks.apk ]; then
+	rm /system/priv-app/STweaks.apk;
+fi;
+if [ -e /system/app/re.codefi.savoca.kcal-v1.1.apk ]; then
+	rm /system/app/re.codefi.savoca.kcal-v1.1.apk;
+fi;
+
 dd if=/tmp/boot.img of=/dev/block/platform/msm_sdcc.1/by-name/boot || exit 1
 exit 0
