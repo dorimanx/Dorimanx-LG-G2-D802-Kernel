891cf69c5866128fe22b784b24cd88650ffed1a0
JT <jututt@users.noreply.github.com>
2018-12-26 18:31:09 +0000

    set min brightness below 60 3%

--- drivers/video/backlight/lm3630.c
+++ drivers/video/backlight/lm3630.c
@@ -1,4 +1,4 @@
-/* drivers/video/backlight/lm3630_bl.c
+/* drivers/video/backlight/lm3630_bl.c JT
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
